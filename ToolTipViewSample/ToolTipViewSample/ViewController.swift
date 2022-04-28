2269ab08353de0e2bdcb2fcd1e6703a140baf71c
김태현 <taehyk528@gmail.com>
2022-04-28 16:35:12 +0900

    Feat: 툴팁 기본 테스트

--- ToolTipViewSample/ToolTipViewSample/ViewController.swift
+++ ToolTipViewSample/ToolTipViewSample/ViewController.swift
@@ -7,11 +7,42 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+import EasyTipView
 
+class ViewController: UIViewController, EasyTipViewDelegate {
+    func easyTipViewDidTap(_ tipView: EasyTipView) {
+        print("did tap")
+    }
+    
+    func easyTipViewDidDismiss(_ tipView: EasyTipView) {
+        print("dismiss")
+    }
+    
+
+    @IBOutlet weak var button: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
+        var preferences = EasyTipView.Preferences()
+        preferences.drawing.font = UIFont(name: "Futura-Medium", size: 13)!
+        preferences.drawing.foregroundColor = UIColor.white
+        preferences.drawing.backgroundColor = .black
+        preferences.drawing.arrowPosition = EasyTipView.ArrowPosition.bottom
+
+        /*
+         * Optionally you can make these preferences global for all future EasyTipViews
+         */
+        EasyTipView.globalPreferences = preferences
+
+        
+        EasyTipView.show(animated: true,
+                         forView: self.button,
+                         withinSuperview: self.view,
+                         text: "내 약을 추가하거나 친구에게 약스케줄을 전송할 수 있어요",
+                         preferences: preferences,
+                         delegate: self)
     }
 
 
